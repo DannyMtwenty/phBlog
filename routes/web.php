94b9a3b01084308e9af10f0a7f5b1940b4d0643e
DannyMtwenty <dannywanyoike@gmail.com>
2022-06-08 11:35:30 +0300

    routing testing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/hello', function () {
+    return "<h1>Hello World</h1>";
+});
+
+Route::get('/about', function () {
+    return view('dashboards.about'); 
+});
